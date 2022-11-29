10bf8f112546d71a38eb237a5943b3952060992c
Chao Pang <ChaoPang229@gmail.com>
2022-11-29 11:51:44 -0500

    added more non-linear transformations to the pre-trained resnet classifier

--- models/resnet_pretrained.py
+++ models/resnet_pretrained.py
@@ -4,14 +4,22 @@ import torch.nn.functional as F
 
 
 class FinetuneResnet152(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, num_classes, dropout_rate=0.5):
         super(FinetuneResnet152, self).__init__()
         model = models.resnet152(pretrained=True)
         self._feature_extractor = nn.Sequential(*(list(model.children())[:-4]))
         # Freeze all the weights
         # for _, p in self._feature_extractor.named_parameters():
         #     p.requires_grad = False
-        self._classifier = nn.Linear(512, num_classes)
+        self._classifier = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(128, num_classes)
+        )
 
     def forward(self, x: Tensor) -> Tensor:
         features = self._feature_extractor(x)
