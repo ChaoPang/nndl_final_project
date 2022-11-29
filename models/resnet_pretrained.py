2ffdcf8ded44f0e149d383fbf51bc8b01d45b1bd
Chao Pang <ChaoPang229@gmail.com>
2022-11-29 11:43:57 -0500

    keep one more layer in pretrained resnet

--- models/resnet_pretrained.py
+++ models/resnet_pretrained.py
@@ -7,11 +7,11 @@ class FinetuneResnet152(nn.Module):
     def __init__(self, num_classes):
         super(FinetuneResnet152, self).__init__()
         model = models.resnet152(pretrained=True)
-        self._feature_extractor = nn.Sequential(*(list(model.children())[:-5]))
+        self._feature_extractor = nn.Sequential(*(list(model.children())[:-4]))
         # Freeze all the weights
-        for _, p in self._feature_extractor.named_parameters():
-            p.requires_grad = False
-        self._classifier = nn.Linear(256, num_classes)
+        # for _, p in self._feature_extractor.named_parameters():
+        #     p.requires_grad = False
+        self._classifier = nn.Linear(512, num_classes)
 
     def forward(self, x: Tensor) -> Tensor:
         features = self._feature_extractor(x)
